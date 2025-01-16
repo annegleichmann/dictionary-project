1cce39550eb915fc0087789cbcd6c6074a48f0f1
annegleichmann <anne.gleichmann@web.de>
2025-01-16 17:18:31 +0100

    added phonetics

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,10 @@ export default function Result(props) {
   if (props.result) {
     return (
       <div className="Result">
-        <h2>{props.result.word}</h2>
+        <section>
+          <h2>{props.result.word}</h2>
+          <p className="phonetics">{props.result.phonetic}</p>
+        </section>
         {props.result.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
